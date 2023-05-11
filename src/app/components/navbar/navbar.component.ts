534486805fd708ea45be42d900e9f49509d48e8e
longlee10 <longquan98@gmail.com>
2023-05-10 23:10:36 -0400

    Adding cart count to nav bar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AppUser } from 'src/app/models/app-user';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,18 +11,34 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 export class NavbarComponent {
   appUser: AppUser;
   isAdmin: boolean;
+  itemCount: number;
 
-  constructor(public auth: AuthenticationService) {
-    // afAuth.authState.subscribe((user) => {
-    //   this.user = user;
-    // });
-    auth.appUser$.subscribe((appUser) => {
+  constructor(
+    private auth: AuthenticationService,
+    private cartService: ShoppingCartService
+  ) {}
+  // afAuth.authState.subscribe((user) => {
+  //   this.user = user;
+  // });
+
+  async ngOnInit() {
+    this.auth.appUser$.subscribe((appUser) => {
       if (appUser) {
         this.appUser = appUser;
         this.isAdmin = this.appUser.isAdmin;
       }
     });
+
+    //Displaying number of items in cart on the nav bar
+    let cart$ = (await this.cartService.getCart())
+      .valueChanges()
+      .subscribe((cart: any) => {
+        this.itemCount = 0;
+        for (let productId in cart.items)
+          this.itemCount += cart.items[productId].quantity;
+      });
   }
+
   logout() {
     this.auth.logout();
   }
